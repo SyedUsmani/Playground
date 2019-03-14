c9792ea4a40978003ebf8a1d5e83c2b2af9f58cb
SyedUsmani <48212879+SyedUsmani@users.noreply.github.com>
2019-03-14 09:42:26 +0530

    Test code pushed to repository.

---
+++ Reverse a 3 digit number/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String args[])
+  {
+    //Type your code here
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int first_dig = n/100;
+    int second_dig = ((n/10)%10);
+    int last_dig = n%10;
+    int reverse = (last_dig*100)+(second_dig*10)+(first_dig);
+    System.out.println(reverse);
+  }
+}
